8ae803ccf6320358f2d69a444ca298cde864e545
Nick Kirkby <nickkirkby@protonmail.ch>
2021-08-04 12:27:24 +0800

    add instructions on how to reproduce

--- main/main.py
+++ main/main.py
@@ -1,8 +1,22 @@
+"""
+By inspecting *dynamic!* path and breaking with ipdb, I can remove
+the pygame .dlls in question before
+```
+import pygame, sys
+from pygame.locals import *
+```
+This results in the same error seen with PyQt6: ImportError: DLL load failed
+"""
+# import sys
+# print("\n".join(sys.path))
+# import ipdb; ipdb.set_trace()
 
 import pygame, sys
 from pygame.locals import *
 
 pygame.init()
+
+
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 while True: # main game loop
