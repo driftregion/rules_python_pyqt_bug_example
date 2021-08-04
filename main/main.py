abc1e652804dd03b5152b2189c0613ee093decbb
Nick Kirkby <nickkirkby@protonmail.ch>
2021-08-04 12:20:58 +0800

    loading pygame DLLs seems to work

--- main/main.py
+++ main/main.py
@@ -1,7 +1,13 @@
-from PyQt6.QtWidgets import QApplication, QLabel
 
-app = QApplication([])
-label = QLabel("Hello World!")
-label.show()
+import pygame, sys
+from pygame.locals import *
 
-app.exec()
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
